535d46fb43342a44b9f514588b8670d6c97b0c19
root <root@boomtownvm.us-central1-a.c.boomtown-launch.internal>
2021-03-11 23:48:29 +0000

    implemented javascript that handles dynamic T-combinator

--- web_dynamic/templates/scripts/index.js
+++ web_dynamic/templates/scripts/index.js
@@ -15,10 +15,17 @@ $(document).ready(function () {
 
 function populate (data) {
   let i;
-  console.log('Company names in our database:');
-
+  const out = $('ul.slides');
+  out.empty();
   for (i = 0; i < data.length; i++) {
-    console.log(data[i].name);
+    let inner = `<li class='slide'><div class='slide-content'><h2 class='slide-title'>${data[i].name}</h2>`;
+    inner += `<p class='industries'><strong>Industry: </strong>${data[i].industries}</p>`;
+    inner += `<p class='tech-stack'><strong>Tech Stack: </strong>${data[i].tech_stack}</p>`;
+    inner += `<p class='employees'><strong>Number of Employees: </strong>${data[i].employee_count}</p>`;
+    inner += `<p class='co-link'><strong>Website: </strong><a href='${data[i].website}'>${data[i].website}</a></p>`;
+    inner += `<p class='job-link'><strong>Job Board Link: </strong><a href='${data[i].jobs_board}'>${data[i].jobs_board}</a></p></div>`;
+    inner += `<p class='company-logo'><img src='images/logos/${data[i].logo_file}' alt='${data[i].name} Logo'></p></li>`;
+    out.append(inner);
   }
 }
 
