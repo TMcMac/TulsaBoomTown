ead4f9eb0830993466a8e6193720026c2e4da6ca
root <root@boomtownvm.us-central1-a.c.boomtown-launch.internal>
2021-03-11 23:07:13 +0000

    added /jobs api route

--- models/job.py
+++ models/job.py
@@ -9,7 +9,13 @@ from sqlalchemy import Column, String
 class Job(BaseModel, Base):
     """ representation of a Company """
     __tablename__ = "remote_jobs"
-    name = Column(String(128), nullable=False)
+    title = Column(String(100), nullable=True)
+    company_name = Column(String(100), nullable=True)
+    job_type = Column(String(100), nullable=True)
+    category = Column(String(100), nullable=True)
+    url = Column(String(230), nullable=True)
+    publication_date = Column(String(100), nullable=True)
+    candidate_required_location = Column(String(200), nullable=True)
 
     def __init__(self, *args, **kwargs):
         """ constructor """
